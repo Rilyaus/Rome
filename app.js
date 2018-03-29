1ed58eef9eba44506ae998412b51984ddef704a7
Rilyaus <rilyaus.gd@gmail.com>
2018-03-29 18:05:26 +0900

    module setting

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var app     = express();
-var router  = require('./router/main') (app);
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var session     = require('express-session');
+var fs          = require('fs');
+var app         = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+var router      = require('./router/main') (app, fs);
 
 var server  = app.listen(3000, function() {
     console.log("Moneybook server has started on port 3000");
--- router/main.js
+++ router/main.js
@@ -1,4 +1,4 @@
-module.exports = function(app) {
+module.exports = function(app, fs) {
     app.get('/', (req, res) => {
         res.render('index.html');
     });
